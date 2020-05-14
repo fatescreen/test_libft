3333db467671b9035b41eb1bc0267603e54810f2
fatescreen <tshocker@student.21-school.ru>
2020-05-14 11:50:50 +0300

    added test for ft_atoi

---
+++ test/src/test_ft_atoi.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "../../../libft/libft.h"
+}
+
+TEST(test_ft_atoi, test_1) 
+{	
+	ASSERT_EQ(ft_atoi("test"), atoi("test"));	
+	ASSERT_EQ(ft_atoi("123"), atoi("123"));
+	ASSERT_EQ(ft_atoi("457457/46"), atoi("457457/46"));
+	ASSERT_EQ(ft_atoi("dfhdf4365"), atoi("dfhdf4365"));
+	ASSERT_EQ(ft_atoi("sdgsdg32523fhd"), atoi("sdgsdg32523fhd"));
+} 
